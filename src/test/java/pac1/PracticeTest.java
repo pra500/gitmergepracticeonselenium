40a487ecf07aea3456d181e1fb33c904b5e9bb04
pragti gupta <pragtig6@gmail.com>
2024-12-11 13:04:53 +0530

    m1

---
+++ src/test/java/pac1/PracticeTest.java
@@ -0,0 +1,39 @@
+package pac1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class PracticeTest {
+
+	WebDriver driver;
+
+	@Test
+	public void fTest() {
+
+		System.out.println(driver.getTitle());
+
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		driver.get("https://naveenautomationlabs.com/opencart/");
+	}
+
+	@AfterClass
+	public void afterClass() {
+
+		driver.close();
+
+	}
+}
